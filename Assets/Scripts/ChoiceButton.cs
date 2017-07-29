42b85730aad6af5f430782d7bb52beae4d42244c
Gustaf Andersson <c.gustaf@hotmail.se>
2017-07-29 22:49:20 +0200

    Conversation/Choices rework

---
+++ Assets/Scripts/ChoiceButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceButton : MonoBehaviour 
+{
+    private ConversationHandler m_parent;
+    private int m_choiceId;
+
+    public void getData(ConversationHandler parent, int choiceId)
+    {
+        m_parent = parent;
+        m_choiceId = choiceId;
+    }
+
+    public void Clicked()
+    {
+        m_parent.ContinueConversation(m_choiceId);
+    }
+}
---
+++ Assets/Scripts/ConversationHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationHandler : MonoBehaviour {
+
+    [SerializeField] private Button m_buttoneTemplate;
+    private BaseConvoNode m_conversation;
+
+    public void StartConversation(BaseConvoNode[] possibleConvos, int intimacy)
+    {
+        List<BaseConvoNode> viable = new List<BaseConvoNode>();
+        foreach (BaseConvoNode node in possibleConvos)
+        {
+            if (!node.Passed && intimacy >= node.IntimacyReq)
+                viable.Add(node);
+        }
+
+        BaseConvoNode m_conversation = viable[Random.Range(0, viable.Count - 1)];
+
+        foreach (string btnName in m_conversation.Choices)
+            Instantiate(m_buttoneTemplate, transform).GetComponentInChildren<Text>().text = btnName;
+    }
+
+    public void ContinueConversation(int answer)
+    {
+        ClearChildren();
+    }
+
+    private void ClearChildren()
+    {
+        foreach (Transform child in transform)
+            Destroy(child);
+    }
+}
