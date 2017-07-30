de42b652b4806cbacbe2df617157dd478bacdeb7
Gustaf Andersson <c.gustaf@hotmail.se>
2017-07-30 20:50:04 +0200

    FUCK XML

--- Assets/Scripts/ConvoNode.cs
+++ Assets/Scripts/ConvoNode.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [System.Serializable]
 public class ConvoNode
@@ -46,4 +47,78 @@ public class ConvoNode
         get{ return m_negativeResponse; }
     }
 
+    public ConvoNode(string voiceline, string[] choices, int[] correct, int[] neutral, int[] negative, 
+        ConvoNode posRes = null, ConvoNode netRes = null, ConvoNode negRes = null)
+    {
+        m_voiceLine = voiceline;
+        m_choices = choices;
+        m_correct = correct;
+        m_neutral = neutral;
+        m_negative = negative;
+        m_positiveResponse = posRes;
+        m_neutralResponse = netRes;
+        m_negativeResponse = negRes;
+    }
+
+    public ConvoNode(){}
+
+}
+
+[System.Serializable]
+public class BaseConvoNode : ConvoNode
+{
+    [SerializeField] private int m_reqIntimacy;
+    public int IntimacyReq
+    {
+        get{ return m_reqIntimacy; }
+    }
+    [SerializeField] private bool m_passed;
+    public bool Passed
+    {
+        get{ return m_passed; }
+    }
+
+    private ConvoNode activeNode;
+
+    /// <summary>
+    /// Set active conversation node based on answer.
+    /// </summary>
+    /// <param name="answer">Answer.</param>
+    public void Answer(int answer)
+    {
+        if (activeNode.CorrectAnswers.Contains(answer))
+        {
+            activeNode = activeNode.PositiveResponse;
+
+            // If conversation has ended on positive, flag as passed.
+            if (activeNode.Choices.Length == 0)
+                m_passed = true;
+        }
+        else if (activeNode.NeutralAnswers.Contains(answer))
+            activeNode = activeNode.NeutralResponse;
+        else if (activeNode.NegativeAnswers.Contains(answer))
+            activeNode = activeNode.NegativeResponse;
+        else
+        {
+            Debug.Log("Error: Impossible answer given.");
+        }
+    }
+
+    public BaseConvoNode(int reqIntimacy, string voiceline, string[] choices, int[] correct, int[] neutral, int[] negative, 
+        ConvoNode posRes = null, ConvoNode netRes = null, ConvoNode negRes = null, bool passed = false)
+    {
+        m_voiceLine = voiceline;
+        m_choices = choices;
+        m_correct = correct;
+        m_neutral = neutral;
+        m_negative = negative;
+        m_positiveResponse = posRes;
+        m_neutralResponse = netRes;
+        m_negativeResponse = negRes;
+
+        m_reqIntimacy = reqIntimacy;
+        m_passed = passed;
+
+        activeNode = this;
+    }
 }
---
+++ Assets/Scripts/XMLTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XMLTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+
+        testReader test = new testReader();
+        test.readXML();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
